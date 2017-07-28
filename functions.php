2a5c5decaa3537ef5b01be678673a3e0d3cb5047
kairongxu <xukairong1991@gmail.com>
2017-07-27 18:57:37 -0700

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Theme Name: Kairong Xu's Summer 2017 WordPress
+Author: Kairong Xu
+Author URI: xukairong.net
+Description: This is a WordPress Theme
+Version: 1.0
+*/
+
+// Register Menus
+register_nav_menus(array('main-menu' => 'Main Menu'));
+//
+?>
